aad2850482c8a49c28702971bef6a55b9bb8b3c2
Accel Maeshiro <accelwtf@gmail.com>
2026-09-17 16:01:31 -0500

    Remove commented code regarding resource file handling in Main.java

--- src/main/java/com/accel/finanzas/Main.java
+++ src/main/java/com/accel/finanzas/Main.java
@@ -6,10 +6,7 @@ import java.util.List;
 import java.util.Properties;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
-        // Los archivos de src/main/resources viajan dentro del .jar y se leen así, desde el
-        // "classpath"
         System.out.println("hola mundo somos amigos");
 
         var config = new Properties();
