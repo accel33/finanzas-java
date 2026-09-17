5f9ae96801c9b16c5adfd12fa7b336fc85b6832e
Accel Maeshiro <accelwtf@gmail.com>
2026-09-17 16:50:02 -0500

    Remove commented note about BigDecimal usage in Movimiento.java

--- src/main/java/com/accel/finanzas/Movimiento.java
+++ src/main/java/com/accel/finanzas/Movimiento.java
@@ -2,5 +2,4 @@ package com.accel.finanzas;
 
 import java.math.BigDecimal;
 
-// Dinero SIEMPRE en BigDecimal, nunca double: double pierde centavos (0.1 + 0.2 = 0.30000000000000004)
 public record Movimiento(String descripcion, BigDecimal monto) {}
